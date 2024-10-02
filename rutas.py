dfc8635a58bd0d7945cf5ca7513464a6d7e27865
Dolopex <dolopex2004@hotmail.com>
2024-10-01 20:10:04 -0500

    rutas

---
+++ rutas.py
@@ -0,0 +1,23 @@
+rutas_transcaribe = {
+    "T100E": {
+        "horarios": {
+            "lunes_viernes": "6 AM - 8 PM",
+            "sabados": "6 AM - 1 PM"
+        },
+        "barrios": [
+            "La Bodeguita", "Centro", "María Auxiliadora", 
+            "Cuatro Vientos", "La Castellana", "Madre Bernarda", "Portal"
+        ]
+    },
+    "T101": {
+        "horarios": {
+            "lunes_viernes": "5 AM - 9:30 PM",
+            "sabados": "5:30 AM - 8:30 PM",
+            "domingos": "6 AM - 8 PM"
+        },
+        "barrios": [
+            "La Bodeguita", "Centro", "María Auxiliadora", 
+            "Cuatro Vientos", "La Castellana", "Madre Bernarda", "Portal"
+        ]
+    }
+}
