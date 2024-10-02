316c1eab95893086d369cd1fdcb883f765ad8fa6
Luis E García p <garciapuelloluiseduardou@gmail.com>
2024-10-01 20:51:35 -0500

    sexo

--- rutas.py
+++ rutas.py
@@ -13,11 +13,45 @@ rutas_transcaribe = {
         "horarios": {
             "lunes_viernes": "5 AM - 9:30 PM",
             "sabados": "5:30 AM - 8:30 PM",
-            "domingos": "6 AM - 8 PM"
+            "domingos": "6 AM - 8 PM",
         },
         "barrios": [
             "La Bodeguita", "Centro", "María Auxiliadora", 
             "Cuatro Vientos", "La Castellana", "Madre Bernarda", "Portal"
         ]
-    }
+    },
+    "X101": {
+        "horarios": {
+            "lunes_viernes": "5 AM - 9 PM",
+            "sabados": "5:30 AM - 8:30 PM",
+            "domingos": "6 AM - 8 PM",
+        },
+        "barrios": [
+            "La Bodeguita", "Centro", "Chambacù", 
+            "Bazurto", "María Auxiliadora", "España", "República del Líbano",
+            "Cuatro Vientos","Villa Olímpica","Los Ejecutivos"
+        ]
+    },
+    "X102": {
+        "horarios": {
+            "lunes_viernes": "5 AM - 9 PM",
+            "sabados": "5:30 AM - 8:30 PM",
+            "domingos": "6 AM - 8 PM",
+        },
+        "barrios": [
+            "La Bodeguita", "Centro", "Lo Amador", 
+            "Delicias", "Bazurto", "Madre Bernarda", "Portal"
+        ]
+    },
+    "X104": {
+        "horarios": {
+            "lunes_viernes": "5 AM - 9 PM",
+            "sabados": "5:30 AM - 8:30 PM",
+            "domingos": "6 AM - 8 PM",
+        },
+        "barrios": [
+            "La Bodeguita", "Centro", "Chambacú", 
+            "La Popa", "Bazurto",
+        ]
+    },
 }
